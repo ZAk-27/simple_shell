c0f432ea3db09e73a506417c6a2180c6f085fbd4
ZAk-27 <touhadi.zakaria@gmail.com>
2023-07-19 11:46:02 -0700

    update

--- main.c
+++ main.c
@@ -1,17 +1,29 @@
 #include "main.h"
 
 int main(int ac, char **argv){
-  char *prompt = "(SZell) $ ";
+  char *prompt = "SZell $ ";
   char *lineptr;
   size_t n = 0; 
+  ssize_t nchars_read;
 
-  /* declaring void variables */
+   /* void variables to declare */
   (void)ac; (void)argv;
 
-  printf("%s", prompt);
-  getline(&lineptr, &n, stdin);
-  printf("%s\n", lineptr);
+    /* creating infnite loop */
+    while (1){
+      printf("%s", prompt);
+      nchars_read = getline(&lineptr, &n, stdin);
+      /* verify the getline function failure or use CTRL + D or get EOF  */ 
+        if (nchars_read == -1){
+            printf("SHell is exiting..\n");
+            return (-1);
+        }
 
-  free(lineptr);
+      printf("%s\n", lineptr);
+
+      /* allocated memory will be free */    
+    }
+      free(lineptr);
+
   return (0);
 }
